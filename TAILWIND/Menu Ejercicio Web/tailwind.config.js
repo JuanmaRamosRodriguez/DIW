90a8490bb2bb3b57e0545dbed46ddefc42b1cd48
JuanmaRamosRodriguez <95094039+JuanmaRamosRodriguez@users.noreply.github.com>
2023-01-16 19:03:10 +0100

    update
    Avance del menú

--- TAILWIND/Menu Ejercicio Web/tailwind.config.js
+++ TAILWIND/Menu Ejercicio Web/tailwind.config.js
@@ -1,10 +1,22 @@
 module.exports = {
-        content: [
-          './components/**/*.{html,js}',
-          './pages/**/*.{html,js}',
-          './index.html',
-        ],
-        // ...
-      }
-    
-    
+  variants: {
+    extend: {
+        animation: ['responsive','hover','group-hover'],
+        fontSize: ['responsive','hover','group-hover'],
+        transform: ['responsive','hover','group-hover'],
+        scale: ['responsive','hover','group-hover'],
+    }
+  },
+
+  content: [ './components//.{html,js}',
+              './pages/**/.{html,js}',
+              './index.html',],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
